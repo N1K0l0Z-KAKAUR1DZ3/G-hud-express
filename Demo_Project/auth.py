5ddbbbe7af13c6fe11c0fa4538dafb4a46ec2bc9
Nika <n.kakauridze9@gmail.com>
2023-04-08 04:49:01 +0400

    write html and backend fork

--- Demo_Project/auth.py
+++ Demo_Project/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,36 @@ def login():
 def logout():
     return "<p> Logout </p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p> sign up </p>"
+    if request.method == "POST":
+        firstName = request.form.get('firstName')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        number = request.form.get('number')
+
+
+       
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # Store user data in database as "pending"
+            new_user = User(firstName=firstName, email=email, password=password1, number=number, status='pending')
+            db.session.add(new_user)
+            db.session.commit()
+            # Redirect to a page indicating that the sign-up request has been received and is pending approval
+            return render_template('pending-approval.html')
+    
+    return render_template('sign-up.html')
+
+
